0839f580126b3e64cd0d12553a9eb6290eb0a9cd
cvetelinamin <cvetelina.min.georgieva@gmail.com>
2023-09-07 23:14:24 +0300

    Added TwoDimensionalArray inserValues

--- TwoDimensionalArray/TwoDimensionalArray/Program.cs
+++ TwoDimensionalArray/TwoDimensionalArray/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using TwoDimensionalArray;
+
+TwoDimensianalArray arr = new TwoDimensianalArray(3, 3);
+
+arr.insertValueInTheArray(0, 0, 10);
+arr.insertValueInTheArray(0, 0, 20);
---
+++ TwoDimensionalArray/TwoDimensionalArray/TwoDimensianalArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArray
+{
+    public class TwoDimensianalArray
+    {
+        int[,] arr;
+
+        public TwoDimensianalArray(int numberOfRows, int numberOfColumns)
+        {
+            this.arr = new int[numberOfRows, numberOfColumns];
+            for (int  row = 0; row < arr.GetLength(0); row++)
+            {
+                for (int col = 0; col < arr.GetLength(1); col++)
+                {
+                    arr[row, col] = int.MinValue;
+                }
+            }
+        }
+
+        // Inserting value - O(1) time and space complexity
+        public void insertValueInTheArray(int row, int col, int value)
+        {
+            try
+            {
+                if (arr[row,col] == int.MinValue)
+                {
+                    arr[row, col] = value;
+                    Console.WriteLine("The value is successfully inserted");
+                } else
+                {
+                    Console.WriteLine("The cell is already occupied");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
