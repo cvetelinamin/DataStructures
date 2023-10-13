ac7435c110cee0019c78e5634357a486b734a6ff
cvetelinamin <cvetelina.min.georgieva@gmail.com>
2023-10-13 22:38:43 +0300

    Added implementation for selection sort

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -13,9 +13,10 @@ foreach (int item in sortedArray)
 
 Console.WriteLine();
 
+ int[] insArray = { 8, 1, 56, 4, 7, 1, 87, 0 };
 InsertionSort ins = new InsertionSort();
 
-int[] sortedArrayIns = ins.Sort(array);
+int[] sortedArrayIns = ins.Sort(insArray);
 
 foreach (int item in sortedArrayIns)
 {
@@ -24,11 +25,23 @@ foreach (int item in sortedArrayIns)
 
 Console.WriteLine();
 
+ int[] mergeArray = { 8, 1, 56, 4, 7, 1, 87, 0 };
 MergeSort ms = new MergeSort();
 
-int[] sortedArrayMerge = ms.Sort(array);
+int[] sortedArrayMerge = ms.Sort(mergeArray);
 
 foreach (int item in sortedArrayIns)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+int[] selArray = { 8, 1, 56, 4, 7, 1, 87, 0 };
+SelectionSort ss = new SelectionSort();
+int[] sortedArraySelection = ss.Sort(selArray);
+
+foreach (int item in sortedArraySelection)
 {
     Console.WriteLine(item);
 }
---
+++ Sorting/Sorting/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class SelectionSort
+    {
+        public int[] Sort(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int min = i;
+                int temp = array[i];
+                for (int j = i+1; j < length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                array[i] = array[min];
+                array[min] = temp;
+            }
+
+            return array;
+
+        }
+    }
+}
